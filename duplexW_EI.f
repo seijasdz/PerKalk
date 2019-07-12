f58e4139bee9f997cff44f074ec3d7792607be4d
José Rául Hernández Seijas <seijasdz@gmail.com>
2019-07-12 00:35:51 -0400

    ee

---
+++ duplexW_EI.f
@@ -0,0 +1,402 @@
+> seq1
+ggggtaagtt
+> seq2
+aaggtattgt
+> seq3
+ttggtgagtg
+> seq4
+caggtatgtg
+> seq5
+caggtaagga
+> seq6
+agggtaagat
+> seq7
+cctgtgagtt
+> seq8
+ctggtgagga
+> seq9
+caggtgggcg
+> seq10
+gcggtgagtt
+> seq11
+gaggtacccg
+> seq12
+caggtacagt
+> seq13
+caggtgatgc
+> seq14
+caggtctgtc
+> seq15
+gaggtgagcg
+> seq16
+acggtgagtg
+> seq17
+ggggtgagtc
+> seq18
+taggtaagag
+> seq19
+caggtagtaa
+> seq20
+caggtaaacg
+> seq21
+tcggtggggg
+> seq22
+ttggtgagct
+> seq23
+ccggtgaggc
+> seq24
+gaggtaggcg
+> seq25
+aaggtgaccg
+> seq26
+aaggtgaggc
+> seq27
+aaggtgaggt
+> seq28
+aaggtgagtg
+> seq29
+gaggtaaaag
+> seq30
+caggtgggcg
+> seq31
+gcggtgagtt
+> seq32
+gaggtacccg
+> seq33
+caggtacagt
+> seq34
+caggtgatgc
+> seq35
+caggtctgtc
+> seq36
+caggtacgtg
+> seq37
+aaggtaatac
+> seq38
+caggtactgg
+> seq39
+caggtgccag
+> seq40
+caggtgagtc
+> seq41
+caggtgaggc
+> seq42
+gaggtgagcg
+> seq43
+acggtgagtg
+> seq44
+ggggtgagtc
+> seq45
+taggtaagag
+> seq46
+caggtagtaa
+> seq47
+caggtaaacg
+> seq48
+tcggtggggg
+> seq49
+ttggtgagct
+> seq50
+ccggtgaggc
+> seq51
+gaggtaggcg
+> seq52
+ctggtacgtt
+> seq53
+tgggtaagcg
+> seq54
+caggtaagcc
+> seq55
+caggtaagat
+> seq56
+ggggtagggg
+> seq57
+caggtacggg
+> seq58
+ctggtgagca
+> seq59
+caggtatgcg
+> seq60
+aaggtgaggc
+> seq61
+caggtgctcc
+> seq62
+caggtaatgt
+> seq63
+agggtgagca
+> seq64
+gaggtgagtg
+> seq65
+aaggtgcgcg
+> seq66
+gaggtgaggc
+> seq67
+aaggtgagcg
+> seq68
+tctgtgagtg
+> seq69
+caggtgaggg
+> seq70
+caggtgagcc
+> seq71
+aaggtaggtc
+> seq72
+caggtgcgta
+> seq73
+caggtactgg
+> seq74
+aaggtccgct
+> seq75
+tctgtgagtg
+> seq76
+aaggtatggc
+> seq77
+ccggtgggtg
+> seq78
+cccgtgagcg
+> seq79
+caggtagtgg
+> seq80
+atggtacggc
+> seq81
+caggtacggg
+> seq82
+ctggtgagta
+> seq83
+atggtaagag
+> seq84
+aaagtatgtg
+> seq85
+acggtaagct
+> seq86
+aaggtgagta
+> seq87
+aaggtaagaa
+> seq88
+ttggtaagct
+> seq89
+caggtacccg
+> seq90
+aaggtaggga
+> seq91
+gaggtatgca
+> seq92
+atagtgagta
+> seq93
+caggtagaca
+> seq94
+caggtaagga
+> seq95
+caggtactcc
+> seq96
+ctggtgagta
+> seq97
+atggtaagag
+> seq98
+aaagtatgtg
+> seq99
+acggtaagct
+> seq100
+atggtaagtg
+> seq101
+caggtaagat
+> seq102
+ccggtgagtg
+> seq103
+aaggtgagtg
+> seq104
+atggtaagga
+> seq105
+ggggtaaggc
+> seq106
+caggtacccg
+> seq107
+aaggtaggga
+> seq108
+gaggtatgca
+> seq109
+atagtgagta
+> seq110
+caggtagaca
+> seq111
+caggtaagga
+> seq112
+caggtactcc
+> seq113
+gaggtgagag
+> seq114
+ctggtaacag
+> seq115
+caggtaagtg
+> seq116
+atggtaagaa
+> seq117
+aaggttcgtt
+> seq118
+tcggtgagtg
+> seq119
+aaggtacggg
+> seq120
+tcggtgagtg
+> seq121
+aaggtacggg
+> seq122
+aaggtgaagc
+> seq123
+ctggtgagac
+> seq124
+gaggtgcggc
+> seq125
+ggcgtaagtg
+> seq126
+acagtaaggg
+> seq127
+caggtaggtg
+> seq128
+ttggtgagtg
+> seq129
+ccggtaagtt
+> seq130
+caggtgagag
+> seq131
+atggtgagga
+> seq132
+caagtgaggg
+> seq133
+gcggtgagtg
+> seq134
+gaggtatacc
+> seq135
+aaggtgggtg
+> seq136
+actgtgagtc
+> seq137
+ccggtgaggg
+> seq138
+gaggtgagtg
+> seq139
+gaggtaagtg
+> seq140
+aaggtgaagc
+> seq141
+ctggtgagac
+> seq142
+aaggtgaagc
+> seq143
+ctggtgagac
+> seq144
+gaggtgagct
+> seq145
+aaggtatgcg
+> seq146
+gaggtgcagc
+> seq147
+caagtaaggg
+> seq148
+taggtgaggg
+> seq149
+agagtgagtc
+> seq150
+acggtgggtc
+> seq151
+tcggtaatgg
+> seq152
+caggtgaccc
+> seq153
+aaggtaagac
+> seq154
+tctgtaagta
+> seq155
+aaggtaagac
+> seq156
+tctgtaagta
+> seq157
+aaggtaacag
+> seq158
+ttggtaaagc
+> seq159
+aaggtatcag
+> seq160
+caggtagggc
+> seq161
+caggtagggc
+> seq162
+gaggtgagtg
+> seq163
+agggtaggtg
+> seq164
+ttggtgagtc
+> seq165
+taggtacgcg
+> seq166
+ctggtgagag
+> seq167
+agggtaaggc
+> seq168
+caggtgagct
+> seq169
+taggtgcgta
+> seq170
+accgtaagtg
+> seq171
+gaggtgaggc
+> seq172
+acggtgaggg
+> seq173
+ccagtgagtg
+> seq174
+tgagtgaggg
+> seq175
+aaggtgagga
+> seq176
+gaggtgagcc
+> seq177
+caggtaggcc
+> seq178
+gaagtgaggg
+> seq179
+caggtaagca
+> seq180
+gttgtattct
+> seq181
+gaggtgagtg
+> seq182
+agggtaggtg
+> seq183
+ttggtgagtc
+> seq184
+caggtgcgct
+> seq185
+gtggtgagta
+> seq186
+ttggtgagtc
+> seq187
+ctggtaagta
+> seq188
+aaggtaaggc
+> seq189
+caggtgagct
+> seq190
+aaggtatgta
+> seq191
+accgtaagtg
+> seq192
+gaggtgaggc
+> seq193
+acggtgaggg
+> seq194
+ccagtgagtg
+> seq195
+tgagtgaggg
+> seq196
+aaggtgagga
+> seq197
+gaggtgagcc
+> seq198
+caggtaggcc
+> seq199
+gaagtgaggg
+> seq200
+caggtaagca
+> seq201
+gttgtattct

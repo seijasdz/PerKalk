8a33e431f74c3a8588181592776988588c0f2502
José Rául Hernández Seijas <seijasdz@gmail.com>
2019-07-26 00:58:15 -0400

    work

--- tss_zoner.py
+++ tss_zoner.py
@@ -9,10 +9,16 @@ with open(atg) as atg_file, open(gen) as cuts_file:
 
     for line in atg_file:
         line2 = line.replace('\n', '')
+        found = False
         for ct in cuts:
+            ct2 = ct.replace('p', '')
             times = 0
-            if line2 in ct:
-                times += 1
-
+            if line2 in ct2:
+                found = True
+                print(ct.index('p'))
+                print(ct2[25: ct2.index(line2) + len(line2)-6])
+        if not found:
+            pass
+            # print(line2)
 
 

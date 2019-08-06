f117fadf1f45004220a626805bbda7e88d31390b
José Rául Hernández Seijas <seijasdz@gmail.com>
2019-08-06 01:06:50 -0400

    tester in work

--- multitester.py
+++ multitester.py
@@ -15,13 +15,17 @@ def divider(elements, sequence, before, after):
             sections
         )
 
-    def get_from(seq):
+    def get_from(seq, before, after):
         return lambda parts: {
-            'gene': seq[int(parts[0][0]) - 1 - before: int(parts[-1][1]) + after].lower(),
+            'gene': seq[parts[0][0] - before: parts[-1][1] + after].lower(),
             'parts': parts,
             'before': before,
             'after': after,
         }
+
+    def correct_cut(cut):
+        edges = cut.split('..')
+        return [int(edges[0]) - 1, int(edges[1])]
 
     trimmed_elements = map(trim, elements)
     tokens = map(lambda string: string.split('/'), trimmed_elements)
@@ -31,8 +35,8 @@ def divider(elements, sequence, before, after):
     no_letters = filter(lambda s: 'A' not in s and 'Z' not in s and 'U' not in s, normals)
     clean_normals = clean(no_letters)
     pre_tuples = map(lambda line: line.split(','), clean_normals)
-    tuples = map(lambda inner: list(map(lambda pre: pre.split('..'), inner)), pre_tuples)
-    full_genes = map(get_from(sequence), tuples)
+    tuples = map(lambda inner: list(map(correct_cut, inner)), pre_tuples)
+    full_genes = map(get_from(sequence, before, after), tuples)
 
     return list(full_genes)
 
@@ -75,9 +79,39 @@ def extract(folder_path, lookfor, before, after):
     return streamline(divided)
 
 
-def test(model):
+def test(model, valid_states):
+    def corrector_generator(difference, before):
+        def correct(tup):
+            return tup[0] - difference + before, tup[1] - difference + before
+        return correct
+
+    def annotator_generator(path):
+        def annotate(cut):
+            return [x[1].name for x in path[cut[0] - 1: cut[1] - 1]]
+        return annotate
+
+    def validator_generator(valid_values):
+
+        def counter(annotated, index, count):
+            if index >= len(annotated):
+                return count
+            if annotated[index]:
+                return counter(annotated, index + 1, count + 1)
+            else:
+                return counter(annotated, index + 1, count)
+
+        def validator(annotated):
+            print(counter(annotated, 0, 0) / len(annotated))
+        return validator
+
     def predict(data):
-        return model.viterbi(converter_to(data['gene'], 2))
+        logp, path = model.viterbi(converter_to(data['gene'], 2))
+        corrected_cuts = map(corrector_generator(data['parts'][0][0], data['before']), data['parts'])
+        annotated = map(annotator_generator(path), corrected_cuts)
+        percent = map(validator_generator(valid_states), annotated)
+        list(percent)
+
+
     return predict
 
 
@@ -86,10 +120,13 @@ if __name__ == '__main__':
         model_json = base_model_file.read()
 
     hmmodel = HiddenMarkovModel.from_json(model_json)
-    genes = extract(folder_path='/run/media/zippyttech/BE96A68C96A6452D/Asi/DataEx/', lookfor='CDS', before=10, after=10)
+    genes = extract(folder_path='/run/media/jose/BE96A68C96A6452D/Asi/DataEx/', lookfor='CDS', before=50, after=10)
+    valid_st = ["zone", "coding"]
+    predicted = map(test(hmmodel, valid_st), genes)
 
-    predicted = map(test(hmmodel), genes)
-    paths = map(lambda p: p[1], predicted)
-    states = map(lambda p: p[1], paths)
-    # names = map(lambda p: [1])
-    print(list(predicted))
+
+
+    #paths = map(lambda x: x[1], predicted)
+    #states = map(lambda lista_de_tuplas: list(map(lambda tupla: tupla[1].name, lista_de_tuplas)), paths)
+    xerxes = list(predicted)
+    print(xerxes)

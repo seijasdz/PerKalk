d41ae76434195a7c696ccc7ada02795dd7e04527
José Rául Hernández Seijas <seijasdz@gmail.com>
2019-10-09 23:33:50 -0400

    continuando

--- full_predictor_tester.py
+++ full_predictor_tester.py
@@ -3,19 +3,37 @@ from xml.etree import ElementTree
 from gene_ebi_to_string import to_string
 
 
+def cut(tag, element, file_string):
+    tokens = element.text.replace('\n', '').replace('\t', '').split('/')
 
-def test(mode, folder):
+
+def get_lines(tag, annotations, file_string):
+    return [cut(tag, annotation, file_string) for annotation in annotations]
+
+
+def test(tag, folder):
     path = Path(folder)
     sub_folders = [x for x in path.iterdir() if x.is_dir()]
+    datas = []
     for folder in sub_folders:
         xml = folder / 'Header.ebi.xml'
         sequence_body = folder / 'sequence_body.ebi'
         if xml.exists() and sequence_body.exists():
-            print(folder)
             file_string = to_string(sequence_body)
             with xml.open() as file:
                 patch = '<ROOT>' + file.read() + '</ROOT>'
             root = ElementTree.fromstring(patch)
             for ft in root.findall('FT'):
-                elements = ft.findall(lookfor)
-                divider(elements, file_string, lookfor)
+                annotations = ft.findall(tag)
+                datas.append(
+                    {
+                        'annotations': annotations,
+                        'file_string': file_string,
+                        'tag': tag
+                    }
+                )
+    lines = [get_lines(x['tag'], x['annotations'], x['file_string']) for x in datas]
+
+
+if __name__ == '__main__':
+    test('mRNA', '/run/media/jose/BE96A68C96A6452D/Asi/Data/')

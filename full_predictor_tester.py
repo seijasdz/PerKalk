09ef4c4269429085abb800318f153d480ab6f3d0
José Rául Hernández Seijas <seijasdz@gmail.com>
2019-10-15 07:50:49 -0400

    more

--- full_predictor_tester.py
+++ full_predictor_tester.py
@@ -3,12 +3,77 @@ from xml.etree import ElementTree
 from gene_ebi_to_string import to_string
 
 
-def cut(tag, element, file_string):
+def reverse(s):
+    return s[::-1]
+
+
+def first_replace(s):
+    s = s.replace('A', 'W').replace('C', 'X').replace('G', 'Y').replace('T', 'Z')
+    return s
+
+
+def second_replace(s):
+    s = s.replace('W', 'T').replace('X', 'G').replace('Y', 'C').replace('Z', 'A')
+    return s
+
+def intron_cut_generator(divs, before, after):
+    new_divs = []
+    for i, div in enumerate(divs):
+        if i > 0:
+            intron = (divs[i - 1][1], div[0])
+            new_divs.append((intron, 'no-an'))
+        new_divs.append((div, 'an'))
+    new_divs.insert(0, ((divs[0][0] - before, divs[0][0]), 'before'))
+    new_divs.append(((divs[-1][1], divs[-1][1] + after), 'after'))
+    print(new_divs)
+
+def slicer(string, divisions, complement, before=25, after=25):
+    if not complement:
+        if divisions:
+            print(divisions)
+            intron_cut_generator(divisions, before, after)
+            start = divisions[0][0] - before
+            end = divisions[-1][1] + after
+            section_with_gene = string[start:end]
+            pure_cuts = [string[div[0]: div[1]] for div in divisions]
+    else:
+        if divisions:
+            start = divisions[0][0] - after
+            end = divisions[-1][1] + before
+            section_with_gene = second_replace(first_replace(reverse(string[start:end])))
+            print(section_with_gene[:28], section_with_gene[-28:])
+
+def transform(section):
+    tokens = section.split('..')
+    real_section = (int(tokens[0]) - 1, int(tokens[1]))
+    return real_section
+
+
+def get_divisions(cut_annotations):
+    cut_pairs = []
+    complement = False
+    if any(x in cut_annotations for x in ['A', 'Z', 'U']):
+        return complement, cut_pairs
+    elif 'complement' in cut_annotations:
+        complement = True
+    cut_annotations = cut_annotations.replace('join', '').replace('(', '').replace(')', '').replace('complement', '')
+    pre_tuples = cut_annotations.split(',')
+    for pre_tuple in pre_tuples:
+        real_tuple = transform(pre_tuple)
+        cut_pairs.append(real_tuple)
+    return complement, cut_pairs
+
+
+def cut(element, file_string):
     tokens = element.text.replace('\n', '').replace('\t', '').split('/')
+    cut_annotations = tokens[0]
+    gene_id = tokens[1]
+    complement, cut_pairs = get_divisions(cut_annotations)
+    slicer(file_string, cut_pairs, complement)
 
 
-def get_lines(tag, annotations, file_string):
-    return [cut(tag, annotation, file_string) for annotation in annotations]
+def get_lines(annotations, file_string):
+    return [cut(annotation, file_string) for annotation in annotations]
 
 
 def test(tag, folder):
@@ -28,12 +93,10 @@ def test(tag, folder):
                 datas.append(
                     {
                         'annotations': annotations,
-                        'file_string': file_string,
-                        'tag': tag
+                        'file_string': file_string
                     }
                 )
-    lines = [get_lines(x['tag'], x['annotations'], x['file_string']) for x in datas]
-
+    lines = [get_lines(x['annotations'], x['file_string']) for x in datas]
 
 if __name__ == '__main__':
-    test('mRNA', '/run/media/jose/BE96A68C96A6452D/Asi/Data/')
+    test('CDS', '/run/media/jose/BE96A68C96A6452D/Asi/Data/')
